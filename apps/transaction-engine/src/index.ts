f1b43109dcd2abb23a52cdfc4899580cc1f6cbd5
Mandeep <msingh0781@gmail.com>
2026-05-07 13:22:55 +0200

    Implement transaction simulation engine

--- apps/transaction-engine/src/index.ts
+++ apps/transaction-engine/src/index.ts
@@ -1,6 +1,46 @@
 import express from "express";
 import cors from "cors";
 
+import { generateTransaction } from "./services/transactionGenerator";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Node Sentinel AI Transaction Engine 🚀");
+});
+
+//Generate a single simulated transaction
+
+app.get("/transactions/generate", (req, res) => {
+  const transaction = generateTransaction();
+
+  res.json(transaction);
+});
+
+//Generate multiple simulated transactions
+
+app.get("/transactions/generate-many", (req, res) => {
+  const transactions = [];
+
+  for (let i = 0; i < 20; i++) {
+    transactions.push(generateTransaction());
+  }
+
+  res.json(transactions);
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Transaction Engine running on http://localhost:${PORT}`);
+});
+
+/* import express from "express";
+import cors from "cors";
+
 const app = express();
 
 app.use(cors());
@@ -15,3 +55,4 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Transaction Engine running on http://localhost:${PORT}`);
 });
+ */
