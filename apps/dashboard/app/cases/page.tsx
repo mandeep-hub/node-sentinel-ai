21cab7f6be55c7eef8c9ad341b76702fc949de2d
Krystle <krystle.leslie.co@gmail.com>
2026-05-14 17:36:15 +0200

    installed missing table components

--- apps/dashboard/app/cases/page.tsx
+++ apps/dashboard/app/cases/page.tsx
@@ -3,6 +3,7 @@ import CasesView from "./CasesView";
 export default async function CasesPage() {
   const response = await fetch(`http://localhost:3000/api/cases`);
   const cases = await response.json();
+  console.log("Cases:", cases);
   return (
     <div>
       <CasesView cases={cases} />
